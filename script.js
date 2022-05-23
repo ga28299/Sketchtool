9a55c2b307fb5b52c4191a4228fe737829e47170
ga28299 <100487946+ga28299@users.noreply.github.com>
2022-05-23 19:31:55 -0400

    Almost done Added color changing, erasing and resetting functionality.

---
+++ script.js
@@ -0,0 +1,69 @@
+let currentColor = "#333333";
+let currentSize = 8;
+let currentMode = "colorFill";
+let slider = document.querySelector("#Size");
+let mouseDown = false;
+document.querySelector(".clear").addEventListener("click", clear);
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+slider.onchange = (e) => changeSize(e.target.value);
+document.querySelector(".eraser").onclick = () => setMode("eraser");
+document.querySelector(".colorPicker").oninput = (e) =>
+  setColor(e.target.value);
+
+function clear() {
+  document.querySelector(".canvas").innerHTML = "";
+  createCanvas(currentSize);
+}
+
+function setColor(color) {
+  currentColor = color;
+}
+
+function setSize(size) {
+  currentSize = size;
+}
+
+function setMode(mode) {
+  currentMode = mode;
+}
+
+let grid = document.querySelector(".canvas");
+
+function changeSize(value) {
+  setSize(value);
+  document.querySelector("#sizeValue").innerHTML = `${value}x${value}`;
+  clear();
+}
+
+function childGen(sizepx) {
+  let child = document.createElement("div");
+  child.classList.add("box");
+  child.style.width = `${sizepx}px`;
+  child.style.height = `${sizepx}px`;
+  return child;
+}
+
+function createCanvas(size) {
+  for (let i = 0; i < size; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    grid.appendChild(row);
+    for (let j = 0; j < size; j++) {
+      row.appendChild(childGen(grid.clientWidth / size));
+      row.addEventListener("mouseover", paint);
+      row.addEventListener("mousedown", paint);
+    }
+  }
+}
+function paint(e) {
+  if (e.type === "mouseover" && !mouseDown) return;
+  if (currentMode === "eraser") {
+    e.target.style.backgroundColor = "#fefefe";
+  } else if (currentMode === "colorFill") {
+    e.target.style.backgroundColor = currentColor;
+  }
+}
+
+createCanvas(currentSize);
